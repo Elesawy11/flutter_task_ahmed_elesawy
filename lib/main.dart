0afd6633187026bdbf5ae4e24cd5ecd1a828ac2d
AhmedElesawy <elesawy928@gmail.com>
2025-10-15 15:57:19 +0300

    base structure of main file

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,32 @@
+import 'package:device_preview/device_preview.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_task/core/responsive/size_helper_extention.dart';
+import 'package:flutter_task/core/responsive/size_provider.dart';
+import 'core/routing/router.dart';
 
-void main(List<String> args) {
-  runApp( const FlutterTask());
+void main() {
+  runApp(
+    DevicePreview(enabled: true, builder: (context) => const FlutterTask()),
+  );
 }
+
 class FlutterTask extends StatelessWidget {
   const FlutterTask({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World'),
+    return SizeProvider(
+      baseSize: Size(360, 690),
+      width: context.screenWidth,
+      height: context.screenHeight,
+      child: MaterialApp.router(
+        debugShowCheckedModeBanner: false,
+        routerConfig: AppRouter.router,
+        theme: ThemeData(
+          fontFamily: 'Tajawal',
+          scaffoldBackgroundColor: Colors.white,
         ),
       ),
-      debugShowCheckedModeBanner: false,
     );
   }
 }
