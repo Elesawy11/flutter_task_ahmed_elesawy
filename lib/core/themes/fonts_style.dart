10574e51d7a03b4d109a7d95d00a4e61408da755
AhmedElesawy <elesawy928@gmail.com>
2025-10-17 22:03:06 +0300

    create font style for app

---
+++ lib/core/themes/fonts_style.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_task/core/responsive/size_helper_extention.dart';
+
+extension FontsStyle on BuildContext {
+  TextStyle get font16w500 =>
+      TextStyle(fontSize: setSp(16), fontWeight: FontWeight.w500);
+  TextStyle get font16w700 =>
+      TextStyle(fontSize: setSp(16), fontWeight: FontWeight.w700);
+  TextStyle get font14w500 =>
+      TextStyle(fontSize: setSp(14), fontWeight: FontWeight.w500);
+  TextStyle get font12w400 =>
+      TextStyle(fontSize: setSp(12), fontWeight: FontWeight.w400);
+  TextStyle get font12w500 =>
+      TextStyle(fontSize: setSp(12), fontWeight: FontWeight.w500);
+
+  TextStyle get font10w500 =>
+      TextStyle(fontSize: setSp(10), fontWeight: FontWeight.w500);
+  TextStyle get font10w400 =>
+      TextStyle(fontSize: setSp(10), fontWeight: FontWeight.w400);
+}
