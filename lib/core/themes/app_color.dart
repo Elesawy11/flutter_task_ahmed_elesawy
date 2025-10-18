e53792d1f37480c19d906901ce9afe3b9059ebd5
AhmedElesawy <elesawy928@gmail.com>
2025-10-18 19:41:24 +0300

    app colors

--- lib/core/themes/app_color.dart
+++ lib/core/themes/app_color.dart
@@ -2,14 +2,16 @@ import 'package:flutter/material.dart';
 
 class AppColor {
   AppColor._();
-  static  Color gray05 = Colors.black.withValues(alpha: 0.5);
+  static Color gray05 = Colors.black.withValues(alpha: 0.5);
+  static Color gray3 = Colors.white.withValues(alpha: 0.3);
   static Color primary = const Color(0xFF090F1F);
   static Color gray09 = primary.withValues(alpha: 0.5);
+  static Color gray10 = const Color(0xFF3B4CF2).withValues(alpha: 0.1);
+
   static Color orange = const Color(0xFFF95B1C);
   static Color orange5 = orange.withValues(alpha: 0.05);
   static Color green = const Color(0xFF3A813F);
   static Color redFF = const Color(0xFFFF4144);
   static Color blue00 = const Color(0xFF0061E2);
   static Color blue1 = blue00.withValues(alpha: 0.1);
-
 }
